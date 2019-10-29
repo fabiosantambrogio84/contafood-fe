413203edbc9f53c1cbcde721e7a93e7750e1f902
Fabio Santambrogio <fabio.santambrogio@ext.contactlab.com>
2019-10-29 14:40:16 +0100

    Add pages for AliquoteIva and TipiPagamento

--- contafood/js/aliquote-iva.js
+++ contafood/js/aliquote-iva.js
@@ -4,17 +4,17 @@ $(document).ready(function() {
 
 	$('#aliquoteIvaTable').DataTable({
 		"ajax": {
-			"url": baseUrl + "aliquote",
+			"url": baseUrl + "aliquote-iva",
 			"type": "GET",
 			"content-type": "json",
 			"cache": false,
 			"dataSrc": "",
 			"error": function(jqXHR, textStatus, errorThrown) {
 				console.log('Response text: ' + jqXHR.responseText);
-				var alertContent = '<div id="alertAgenteContent" class="alert alert-danger alert-dismissible fade show" role="alert">';
-				alertContent = alertContent + '<strong>Errore nel recupero degli agenti</strong>\n' +
+				var alertContent = '<div id="alertAliquotaIvaContent" class="alert alert-danger alert-dismissible fade show" role="alert">';
+				alertContent = alertContent + '<strong>Errore nel recupero delle aliquote iva</strong>\n' +
 					'            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>';
-				$('#alertAgente').empty().append(alertContent);
+				$('#alertAliquotaIva').empty().append(alertContent);
 			}
 		},
 		"language": {
@@ -25,8 +25,8 @@ $(document).ready(function() {
 				"next": "Succ.",
 				"previous": "Prec."
 			},
-			"emptyTable": "Nessun agente disponibile",
-			"zeroRecords": "Nessun agente disponibile"
+			"emptyTable": "Nessuna aliquota iva disponibile",
+			"zeroRecords": "Nessuna aliquota iva disponibile"
 		},
 		"pageLength": 20,
 		"lengthChange": false,
@@ -36,122 +36,110 @@ $(document).ready(function() {
 			[0, 'asc']
 		],
 		"columns": [
-			{"name": "nome", "data": "nome"},
-			{"name": "cognome", "data": "cognome"},
-			{"name": "telefono", "data": "telefono"},
-			{"name": "email", "data": "email"},
-			{"name": "indirizzo", "data": "indirizzo"},
+			{"name": "valore", "data": "valore"},
 			{"data": null, "orderable":false, "width":"8%", render: function ( data, type, row ) {
-				var links = '<a class="updateAgente pr-2" data-id="'+data.id+'" href="agenti-edit.html?idAgente=' + data.id + '"><i class="far fa-edit"></i></a>';
-				links = links + '<a class="deleteAgente" data-id="'+data.id+'" href="#"><i class="far fa-trash-alt"></i></a>';
+				var links = '<a class="updateAliquotaIva pr-2" data-id="'+data.id+'" href="aliquote-iva-edit.html?idAliquotaIva=' + data.id + '"><i class="far fa-edit"></i></a>';
+				links = links + '<a class="deleteAliquotaIva" data-id="'+data.id+'" href="#"><i class="far fa-trash-alt"></i></a>';
 				return links;
 			}}
 		]
 	});
 
-	$(document).on('click','.deleteAgente', function(){
-		var idAgente = $(this).attr('data-id');
-		$('#confirmDeleteAgente').attr('data-id', idAgente);
-		$('#deleteAgenteModal').modal('show');
+	$(document).on('click','.deleteAliquotaIva', function(){
+		var idAliquotaIva = $(this).attr('data-id');
+		$('#confirmDeleteAliquotaIva').attr('data-id', idAliquotaIva);
+		$('#deleteAliquotaIvaModal').modal('show');
 	});
 
-	$(document).on('click','#confirmDeleteAgente', function(){
-		$('#deleteAgenteModal').modal('hide');
-		var idAgente = $(this).attr('data-id');
+	$(document).on('click','#confirmDeleteAliquotaIva', function(){
+		$('#deleteAliquotaIvaModal').modal('hide');
+		var idAliquotaIva = $(this).attr('data-id');
 
 		$.ajax({
-			url: baseUrl + "agenti/" + idAgente,
+			url: baseUrl + "aliquote-iva/" + idAliquotaIva,
 			type: 'DELETE',
 			success: function() {
-				var alertContent = '<div id="alertAgenteContent" class="alert alert-success alert-dismissible fade show" role="alert">';
-				alertContent = alertContent + '<strong>Agente</strong> cancellato con successo.\n' +
+				var alertContent = '<div id="alertAliquotaIvaContent" class="alert alert-success alert-dismissible fade show" role="alert">';
+				alertContent = alertContent + '<strong>Aliquota iva</strong> cancellata con successo.\n' +
 					'            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>';
-				$('#alertAgente').empty().append(alertContent);
+				$('#alertAliquotaIva').empty().append(alertContent);
 
-				$('#agentiTable').DataTable().ajax.reload();
+				$('#aliquoteIvaTable').DataTable().ajax.reload();
 			},
 			error: function(jqXHR, textStatus, errorThrown) {
 				console.log('Response text: ' + jqXHR.responseText);
 
-				var alertContent = '<div id="alertAgenteContent" class="alert alert-danger alert-dismissible fade show" role="alert">';
-                alertContent = alertContent + '<strong>Errore</strong> nella cancellazione dell\'agente' +
+				var alertContent = '<div id="alertAliquotaIvaContent" class="alert alert-danger alert-dismissible fade show" role="alert">';
+                alertContent = alertContent + '<strong>Errore</strong> nella cancellazione dell\'aliquota iva' +
                     '            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>';
-                $('#alertAgente').empty().append(alertContent);
+                $('#alertAliquotaIva').empty().append(alertContent);
 
-                $('#agentiTable').DataTable().ajax.reload();
+                $('#aliquoteIvaTable').DataTable().ajax.reload();
 			}
 		});
 	});
 
-	if($('#updateAgenteButton') != null && $('#updateAgenteButton') != undefined){
-		$(document).on('submit','#updateAgenteForm', function(event){
+	if($('#updateAliquotaIvaButton') != null && $('#updateAliquotaIvaButton') != undefined){
+		$(document).on('submit','#updateAliquotaIvaForm', function(event){
 			event.preventDefault();
 
-			var agente = new Object();
-			agente.id = $('#hiddenIdAgente').val();
-			agente.nome = $('#nome').val();
-			agente.cognome = $('#cognome').val();
-			agente.telefono = $('#telefono').val();
-			agente.email = $('#email').val();
-			agente.indirizzo = $('#indirizzo').val();
+			var aliquotaIva = new Object();
+			aliquotaIva.id = $('#hiddenIdAliquotaIva').val();
+			aliquotaIva.valore = $('#valore').val();
 
-			var agenteJson = JSON.stringify(agente);
+			var aliquotaIvaJson = JSON.stringify(aliquotaIva);
 
-			var alertContent = '<div id="alertAgenteContent" class="alert alert-@@alertResult@@ alert-dismissible fade show" role="alert">';
+			var alertContent = '<div id="alertAliquotaIvaContent" class="alert alert-@@alertResult@@ alert-dismissible fade show" role="alert">';
 			alertContent = alertContent + '<strong>@@alertText@@</strong>\n' +
 				'<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>';
 
 			$.ajax({
-				url: baseUrl + "agenti/" + $('#hiddenIdAgente').val(),
+				url: baseUrl + "aliquote-iva/" + $('#hiddenIdAliquotaIva').val(),
 				type: 'PUT',
 				contentType: "application/json",
 				dataType: 'json',
-				data: agenteJson,
+				data: aliquotaIvaJson,
 				success: function(result) {
-					$('#alertAgente').empty().append(alertContent.replace('@@alertText@@','Agente modificato con successo').replace('@@alertResult@@', 'success'));
+					$('#alertAliquotaIva').empty().append(alertContent.replace('@@alertText@@','Aliquota iva modificata con successo').replace('@@alertResult@@', 'success'));
 				},
 				error: function(jqXHR, textStatus, errorThrown) {
-					$('#alertAgente').empty().append(alertContent.replace('@@alertText@@','Errore nella modifica dell\' agente').replace('@@alertResult@@', 'danger'));
+					$('#alertAliquotaIva').empty().append(alertContent.replace('@@alertText@@','Errore nella modifica dell\' aliquota iva').replace('@@alertResult@@', 'danger'));
 				}
 			});
 		});
 	}
 
-	if($('#newAgenteButton') != null && $('#newAgenteButton') != undefined){
-		$(document).on('submit','#newAgenteForm', function(event){
+	if($('#newAliquotaIvaButton') != null && $('#newAliquotaIvaButton') != undefined){
+		$(document).on('submit','#newAliquotaIvaForm', function(event){
 			event.preventDefault();
 
-			var agente = new Object();
-			agente.nome = $('#nome').val();
-			agente.cognome = $('#cognome').val();
-			agente.telefono = $('#telefono').val();
-			agente.email = $('#email').val();
-            agente.indirizzo = $('#indirizzo').val();
+			var aliquotaIva = new Object();
+			aliquotaIva.valore = $('#valore').val();
 
-			var agenteJson = JSON.stringify(agente);
+			var aliquotaIvaJson = JSON.stringify(aliquotaIva);
 
-			var alertContent = '<div id="alertAgenteContent" class="alert alert-@@alertResult@@ alert-dismissible fade show" role="alert">';
+			var alertContent = '<div id="alertAliquotaIvaContent" class="alert alert-@@alertResult@@ alert-dismissible fade show" role="alert">';
 			alertContent = alertContent + '<strong>@@alertText@@</strong>\n' +
 				'<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>';
 
 			$.ajax({
-				url: baseUrl + "agenti",
+				url: baseUrl + "aliquote-iva",
 				type: 'POST',
 				contentType: "application/json",
 				dataType: 'json',
-				data: agenteJson,
+				data: aliquotaIvaJson,
 				success: function(result) {
-					$('#alertAgente').empty().append(alertContent.replace('@@alertText@@','Agente creato con successo').replace('@@alertResult@@', 'success'));
+					$('#alertAliquotaIva').empty().append(alertContent.replace('@@alertText@@','Aliquota iva creata con successo').replace('@@alertResult@@', 'success'));
 				},
 				error: function(jqXHR, textStatus, errorThrown) {
-					$('#alertAgente').empty().append(alertContent.replace('@@alertText@@','Errore nella creazione dell\' agente').replace('@@alertResult@@', 'danger'));
+					$('#alertAliquotaIva').empty().append(alertContent.replace('@@alertText@@','Errore nella creazione dell\' aliquota iva').replace('@@alertResult@@', 'danger'));
 				}
 			});
 		});
 	}
 });
 
-$.fn.extractIdAgenteFromUrl = function(){
+$.fn.extractIdAliquotaIvaFromUrl = function(){
     var pageUrl = window.location.search.substring(1);
 
 	var urlVariables = pageUrl.split('&'),
@@ -161,39 +149,35 @@ $.fn.extractIdAgenteFromUrl = function(){
     for (i = 0; i < urlVariables.length; i++) {
         paramNames = urlVariables[i].split('=');
 
-        if (paramNames[0] === 'idAgente') {
+        if (paramNames[0] === 'idAliquotaIva') {
         	return paramNames[1] === undefined ? null : decodeURIComponent(paramNames[1]);
         }
     }
 }
 
-$.fn.getAgente = function(idAgente){
+$.fn.getAliquotaIva = function(idAliquotaIva){
 
-	var alertContent = '<div id="alertAgenteContent" class="alert alert-danger alert-dismissible fade show" role="alert">';
-	alertContent = alertContent + '<strong>Errore nel recupero dell\' agente.</strong>\n' +
+	var alertContent = '<div id="alertAliquotaIvaContent" class="alert alert-danger alert-dismissible fade show" role="alert">';
+	alertContent = alertContent + '<strong>Errore nel recupero dell\' aliquota iva.</strong>\n' +
     					'<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>';
 
     $.ajax({
-        url: baseUrl + "agenti/" + idAgente,
+        url: baseUrl + "aliquote-iva/" + idAliquotaIva,
         type: 'GET',
         dataType: 'json',
         success: function(result) {
           if(result != null && result != undefined && result != ''){
 
-			$('#hiddenIdAgente').attr('value', result.id);
-			$('#nome').attr('value', result.nome);
-            $('#cognome').attr('value', result.cognome);
-            $('#telefono').attr('value', result.telefono);
-            $('#email').attr('value', result.email);
-            $('#indirizzo').attr('value', result.indirizzo);
+			$('#hiddenIdAliquotaIva').attr('value', result.id);
+			$('#valore').attr('value', result.valore);
 
           } else{
-            $('#alertAgente').empty().append(alertContent);
+            $('#alertAliquotaIva').empty().append(alertContent);
           }
         },
         error: function(jqXHR, textStatus, errorThrown) {
-            $('#alertAgente').empty().append(alertContent);
-            $('#updateAgenteButton').attr('disabled', true);
+            $('#alertAliquotaIva').empty().append(alertContent);
+            $('#updateAliquotaIvaButton').attr('disabled', true);
             console.log('Response text: ' + jqXHR.responseText);
         }
     });
