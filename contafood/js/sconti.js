a502fbaad9b0288a16e21c38bb756c8d24bd3fa3
Fabio Santambrogio <f.santambrogio@widegroup.ch>
2019-12-02 20:06:38 +0100

    Fix Sconti

--- contafood/js/sconti.js
+++ contafood/js/sconti.js
@@ -3,9 +3,9 @@ var baseUrl = "/contafood-be/";
 
 $(document).ready(function() {
 
-	$('#scontiTable').DataTable({
+	$('#scontiFornitoriTable').DataTable({
 		"ajax": {
-			"url": baseUrl + "sconti",
+			"url": baseUrl + "sconti?tipologia=FORNITORE",
 			"type": "GET",
 			"content-type": "json",
 			"cache": false,
@@ -26,8 +26,8 @@ $(document).ready(function() {
 				"next": "Succ.",
 				"previous": "Prec."
 			},
-			"emptyTable": "Nessuno sconto disponibile",
-			"zeroRecords": "Nessuno sconto disponibile"
+			"emptyTable": "Nessuno sconto fornitore disponibile",
+			"zeroRecords": "Nessuno sconto fornitore disponibile"
 		},
 		"pageLength": 20,
 		"lengthChange": false,
@@ -44,16 +44,10 @@ $(document).ready(function() {
                 	return '';
 				}
             }},
-            {"name": "fornitore", "data": null, render: function ( data, type, row ) {
+			{"name": "tipologia", "data": "tipologia"},
+			{"name": "fornitore", "data": null, render: function ( data, type, row ) {
                 if(data.fornitore != null){
 					return data.fornitore.ragioneSociale;
-				} else {
-                	return '';
-				}
-            }},
-            {"name": "articolo", "data": null, render: function ( data, type, row ) {
-                if(data.articolo != null){
-					return data.articolo.descrizione;
 				} else {
                 	return '';
 				}
@@ -77,21 +71,98 @@ $(document).ready(function() {
 			{"name": "valore", "data": "valore"},
 			{"data": null, "orderable":false, "width":"15%", render: function ( data, type, row ) {
 				var links = '<a class="updateSconto pr-2" data-id="'+data.id+'" href="sconti-edit.html?idSconto=' + data.id + '"><i class="far fa-edit" title="Modifica"></i></a>';
-				links = links + '<a class="deleteSconto" data-id="'+data.id+'" href="#"><i class="far fa-trash-alt" title="Elimina"></i></a>';
+				links = links + '<a class="deleteSconto" data-id="'+data.id+'" data-tipologia="'+data.tipologia+'" href="#"><i class="far fa-trash-alt" title="Elimina"></i></a>';
 				return links;
 			}}
 		]
 	});
 
+	$('#scontiArticoliTable').DataTable({
+		"ajax": {
+			"url": baseUrl + "sconti?tipologia=ARTICOLO",
+			"type": "GET",
+			"content-type": "json",
+			"cache": false,
+			"dataSrc": "",
+			"error": function(jqXHR, textStatus, errorThrown) {
+				console.log('Response text: ' + jqXHR.responseText);
+				var alertContent = '<div id="alertScontoContent" class="alert alert-danger alert-dismissible fade show" role="alert">';
+				alertContent = alertContent + '<strong>Errore nel recupero dei clienti</strong>\n' +
+					'            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>';
+				$('#alertSconto').empty().append(alertContent);
+			}
+		},
+		"language": {
+			"search": "Cerca",
+			"paginate": {
+				"first": "Inizio",
+				"last": "Fine",
+				"next": "Succ.",
+				"previous": "Prec."
+			},
+			"emptyTable": "Nessuno sconto articolo disponibile",
+			"zeroRecords": "Nessuno sconto articolo disponibile"
+		},
+		"pageLength": 20,
+		"lengthChange": false,
+		"info": false,
+		"autoWidth": false,
+		"order": [
+			[0, 'asc']
+		],
+		"columns": [
+			{"name": "cliente", "data": null, render: function ( data, type, row ) {
+				if(data.cliente != null){
+					return data.cliente.ragioneSociale;
+				} else {
+					return '';
+				}
+			}},
+			{"name": "tipologia", "data": "tipologia"},
+			{"name": "articolo", "data": null, render: function ( data, type, row ) {
+				if(data.articolo != null){
+					return data.articolo.descrizione;
+				} else {
+					return '';
+				}
+			}},
+			{"name": "dataDal", "data": null, render: function ( data, type, row ) {
+					if(data.dataDal != null){
+						var a = moment(data.dataDal);
+						return a.format('DD/MM/YYYY');
+					} else {
+						return '';
+					}
+				}},
+			{"name": "dataAl", "data": null, render: function ( data, type, row ) {
+					if(data.dataAl != null){
+						var a = moment(data.dataAl);
+						return a.format('DD/MM/YYYY');
+					} else {
+						return '';
+					}
+				}},
+			{"name": "valore", "data": "valore"},
+			{"data": null, "orderable":false, "width":"15%", render: function ( data, type, row ) {
+					var links = '<a class="updateSconto pr-2" data-id="'+data.id+'" href="sconti-edit.html?idSconto=' + data.id + '"><i class="far fa-edit" title="Modifica"></i></a>';
+					links = links + '<a class="deleteSconto" data-id="'+data.id+'" data-tipologia="'+data.tipologia+'" href="#"><i class="far fa-trash-alt" title="Elimina"></i></a>';
+					return links;
+				}}
+		]
+	});
+
 	$(document).on('click','.deleteSconto', function(){
 		var idSconto = $(this).attr('data-id');
+		var tipologia = $(this).attr('data-tipologia');
 		$('#confirmDeleteSconto').attr('data-id', idSconto);
+		$('#confirmDeleteSconto').attr('data-tipologia', tipologia);
 		$('#deleteScontoModal').modal('show');
 	});
 
 	$(document).on('click','#confirmDeleteSconto', function(){
 		$('#deleteScontoModal').modal('hide');
 		var idSconto = $(this).attr('data-id');
+		var tipologia = $(this).attr('data-tipologia');
 
 		$.ajax({
 			url: baseUrl + "sconti/" + idSconto,
@@ -102,7 +173,12 @@ $(document).ready(function() {
 					'            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>';
 				$('#alertSconto').empty().append(alertContent);
 
-				$('#scontiTable').DataTable().ajax.reload();
+				if(tipologia == 'FORNITORE'){
+					$('#scontiFornitoriTable').DataTable().ajax.reload();
+				} else {
+					$('#scontiArticoliTable').DataTable().ajax.reload();
+				}
+
 			},
 			error: function(jqXHR, textStatus, errorThrown) {
 				console.log('Response text: ' + jqXHR.responseText);
@@ -114,26 +190,49 @@ $(document).ready(function() {
 		$(document).on('submit','#updateScontoForm', function(event){
 			event.preventDefault();
 
-			var sconto = new Object();
-			sconto.id = $('#hiddenIdSconto').val();
-			if($('#cliente option:selected').val() != -1){
-                var cliente = new Object();
-                cliente.id = $('#cliente option:selected').val();
-                sconto.cliente = cliente;
-            };
-			if($('#fornitore option:selected').val() != -1){
-                var fornitore = new Object();
-                fornitore.id = $('#fornitore option:selected').val();
-                sconto.fornitore = fornitore;
-            };
-            if($('#articolo option:selected').val() != -1){
-                var articolo = new Object();
-                articolo.id = $('#articolo option:selected').val();
-                sconto.articolo = articolo;
-            };
-			sconto.dataDal = $('#dataDal').val();
-			sconto.dataAl = $('#dataAl').val();
-			sconto.valore = $('#valore').val();
+			var sconti = [];
+			var tipologia = $('#tipologia option:selected').val();
+			if(tipologia == 'FORNITORE'){
+				$.each($('#fornitore option:selected').val(), function(i, item){
+					var sconto = new Object();
+					sconto.id = $('#hiddenIdSconto').val();
+					if($('#cliente option:selected').val() != -1){
+						var cliente = new Object();
+						cliente.id = $('#cliente option:selected').val();
+						sconto.cliente = cliente;
+					};
+					sconto.tipologia = tipologia;
+					var fornitore = new Object();
+					fornitore.id = item;
+					sconto.fornitore = fornitore;
+
+					sconto.dataDal = $('#dataDal').val();
+					sconto.dataAl = $('#dataAl').val();
+					sconto.valore = $('#valore').val();
+
+					sconti.push(sconto);
+				});
+			} else {
+				$.each($('#articolo option:selected').val(), function(i, item){
+					var sconto = new Object();
+					sconto.id = $('#hiddenIdSconto').val();
+					if($('#cliente option:selected').val() != -1){
+						var cliente = new Object();
+						cliente.id = $('#cliente option:selected').val();
+						sconto.cliente = cliente;
+					};
+					sconto.tipologia = tipologia;
+					var articolo = new Object();
+					articolo.id = item;
+					sconto.articolo = articolo;
+
+					sconto.dataDal = $('#dataDal').val();
+					sconto.dataAl = $('#dataAl').val();
+					sconto.valore = $('#valore').val();
+
+					sconti.push(sconto);
+				});
+			}
 
 			var scontoJson = JSON.stringify(sconto);
 
@@ -167,16 +266,20 @@ $(document).ready(function() {
                 cliente.id = $('#cliente option:selected').val();
                 sconto.cliente = cliente;
             };
-            if($('#fornitore option:selected').val() != -1){
-                var fornitore = new Object();
-                fornitore.id = $('#fornitore option:selected').val();
-                sconto.fornitore = fornitore;
-            };
-            if($('#articolo option:selected').val() != -1){
-                var articolo = new Object();
-                articolo.id = $('#articolo option:selected').val();
-                sconto.articolo = articolo;
-            };
+            sconto.tipologia = $('#tipologia option:selected').val();
+            if(sconto.tipologia == 'FORNITORE'){
+				if($('#fornitore option:selected').val() != -1){
+					var fornitore = new Object();
+					fornitore.id = $('#fornitore option:selected').val();
+					sconto.fornitore = fornitore;
+				};
+			} else {
+				if($('#articolo option:selected').val() != -1){
+					var articolo = new Object();
+					articolo.id = $('#articolo option:selected').val();
+					sconto.articolo = articolo;
+				};
+			}
             sconto.dataDal = $('#dataDal').val();
             sconto.dataAl = $('#dataAl').val();
             sconto.valore = $('#valore').val();
@@ -202,6 +305,21 @@ $(document).ready(function() {
 			});
 		});
 	}
+
+	$(document).on('change','#tipologia', function(){
+		var tipologia = $('#tipologia option:selected').val();
+		if(tipologia == 'ARTICOLO'){
+			$('#fornitoreDiv').addClass('d-none');
+			$('#articoloDiv').removeClass('d-none');
+			$('#tipologia option[value="ARTICOLO"]').attr('selected', true);
+			$('#tipologia option[value="FORNITORE"]').removeAttr('selected');
+		} else {
+			$('#fornitoreDiv').removeClass('d-none');
+			$('#articoloDiv').addClass('d-none');
+			$('#tipologia option[value="FORNITORE"]').attr('selected', true);
+			$('#tipologia option[value="ARTICOLO"]').removeAttr('selected');
+		}
+	});
 });
 
 $.fn.getClienti = function(){
@@ -212,7 +330,15 @@ $.fn.getClienti = function(){
 		success: function(result) {
 			if(result != null && result != undefined && result != ''){
 				$.each(result, function(i, item){
-					$('#cliente').append('<option value="'+item.id+'">'+item.ragioneSociale+'</option>');
+					var label = '';
+					var isDittaIndividuale = item.dittaIndividuale;
+					if(isDittaIndividuale){
+						label += item.cognome + ' ' + item.nome;
+					} else {
+						label += item.ragioneSociale;
+					}
+					label += ' - ' + item.partitaIva + ' - '+item.codiceFiscale;
+					$('#cliente').append('<option value="'+item.id+'">'+label+'</option>');
 				});
 			}
 		},
@@ -258,6 +384,28 @@ $.fn.getArticoli = function(){
 	});
 }
 
+$.fn.getTipologieSconti = function(){
+	$.ajax({
+		url: baseUrl + "utils/tipologie-sconti",
+		type: 'GET',
+		dataType: 'json',
+		success: function(result) {
+			if(result != null && result != undefined && result != ''){
+				$.each(result, function(i, item){
+					var selected = '';
+					if(item == 'ARTICOLO'){
+						selected = ' selected';
+					}
+					$('#tipologia').append('<option value="'+item+'" '+selected+'>'+item+'</option>');
+				});
+			}
+		},
+		error: function(jqXHR, textStatus, errorThrown) {
+			console.log('Response text: ' + jqXHR.responseText);
+		}
+	});
+}
+
 $.fn.extractIdScontoFromUrl = function(){
     var pageUrl = window.location.search.substring(1);
 
@@ -289,8 +437,11 @@ $.fn.getSconto = function(idSconto){
 
 			$('#hiddenIdSconto').attr('value', result.id);
 			if(result.cliente != null && result.cliente != undefined){
-                  $('#cliente option[value="' + result.cliente.id +'"]').attr('selected', true);
+				$('#cliente option[value="' + result.cliente.id +'"]').attr('selected', true);
             };
+			if(result.tipologia != null && result.tipologia != undefined){
+				$('#tipologia option[value="' + result.tipologia +'"]').attr('selected', true);
+			}
 			if(result.fornitore != null && result.fornitore != undefined){
                   $('#fornitore option[value="' + result.fornitore.id +'"]').attr('selected', true);
             };
