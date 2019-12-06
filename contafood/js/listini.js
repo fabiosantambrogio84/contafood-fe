6446197c1a220e4af38940539eae6502e10445c3
Fabio Santambrogio <fabio.santambrogio@ext.contactlab.com>
2019-12-06 16:25:43 +0100

    Add details popup for Listino

--- contafood/js/listini.js
+++ contafood/js/listini.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
 
 	$('#listiniTable').DataTable({
 		"processing": true,
-        "serverSide": true,
+        //"serverSide": true,
 		"ajax": {
 			"url": baseUrl + "listini",
 			"type": "GET",
@@ -42,15 +42,9 @@ $(document).ready(function() {
 		"columns": [
             {"name": "tipologia", "data": "tipologia", "visible": false},
 		    {"name": "nome", "data": "nome"},
-			{"name": "listinoRiferimento", "data": null, "orderable":true, render: function ( data, type, row ) {
-                if(data.listinoRiferimento != null){
-                    return data.listinoRiferimento.nome;
-                } else{
-                    return '';
-                }
-            }},
-			{"data": null, "orderable":false, "width":"8%", render: function ( data, type, row ) {
-				var links = '<a class="updateListino pr-2" data-id="'+data.id+'" href="listini-edit.html?idListino=' + data.id + '"><i class="far fa-edit"></i></a>';
+			{"data": null, "orderable":false, "width":"10%", render: function ( data, type, row ) {
+				var links = '<a class="detailsListino pr-2" data-id="'+data.id+'" href="#"><i class="fas fa-info-circle"></i></a>';
+				links = links + '<a class="updateListino pr-2" data-id="'+data.id+'" href="listini-edit.html?idListino=' + data.id + '"><i class="far fa-edit"></i></a>';
 				links = links + '<a class="deleteListino" data-id="'+data.id+'" href="#"><i class="far fa-trash-alt"></i></a>';
 				return links;
 			}}
@@ -96,6 +90,65 @@ $(document).ready(function() {
 		});
 	});
 
+    $(document).on('click','.detailsListino', function(){
+        var idListino = $(this).attr('data-id');
+
+        var alertContent = '<div id="alertFornitoreContent" class="alert alert-danger alert-dismissible fade show" role="alert">';
+        alertContent = alertContent + '<strong>Errore nel recupero del listino.</strong></div>';
+
+        $('#detailsListinoModal').modal('show');
+
+        $('#detailsListinoModalTable').DataTable({
+            "ajax": {
+                "url": baseUrl + "listini/" + idListino + '/listini-prezzi',
+                "type": "GET",
+                "content-type": "json",
+                "cache": false,
+                "dataSrc": "",
+                "error": function(jqXHR, textStatus, errorThrown) {
+                    $('#detailsListinoMainDiv').append(alertContent);
+                }
+            },
+            "language": {
+                "search": "Cerca",
+                "emptyTable": "Nessun listino prezzo disponibile",
+                "zeroRecords": "Nessun listino prezzo disponibile"
+            },
+            "paging": false,
+            "lengthChange": false,
+            "info": false,
+            "order": [
+                [0,'asc'],
+                [1,'asc']
+            ],
+            "autoWidth": false,
+            "columns": [
+                {"name": "articolo", "data": null, render: function ( data, type, row ) {
+                    var result = '';
+                    if(data.articolo != null){
+                        result = data.articolo.codice + ' - ' + data.articolo.descrizione;
+                    }
+                    return result;
+                }},
+                {"name": "fornitore", "data": null,  render: function ( data, type, row ) {
+                    var result = '';
+                    if(data.articolo != null){
+                        if(data.articolo.fornitore != null){
+                            result = data.articolo.fornitore.codice + ' - ' + data.articolo.fornitore.ragioneSociale;
+                        }
+                    }
+                    return result;
+                }},
+                {"name": "prezzo", "data": "prezzo"}
+            ]
+        });
+    });
+
+    $(document).on('click','.closeDetailsListino', function(){
+    		$('#detailsListinoModalTable').DataTable().destroy();
+    		$('#detailsListinoModal').modal('hide');
+    	});
+
 	if($('#updateListinoButton') != null && $('#updateListinoButton') != undefined){
 		$(document).on('submit','#updateListinoForm', function(event){
 			event.preventDefault();
@@ -103,13 +156,7 @@ $(document).ready(function() {
 			var listino = new Object();
 			listino.id = $('#hiddenIdListino').val();
 			listino.nome = $('#nome').val();
-			var listinoRiferimentoId = $('#listinoRiferimento option:selected').val();
-			if(listinoRiferimentoId != null && listinoRiferimentoId != '' && listinoRiferimentoId != '-1'){
-			    var listinoRiferimento = new Object();
-                listinoRiferimento.id = listinoRiferimentoId;
-                listino.listinoRiferimento = listinoRiferimento;
-			}
-            listino.tipologia = $('input[name="tipologia"]:checked').val();
+			listino.tipologia = $('input[name="tipologia"]:checked').val();
 
 			var listinoJson = JSON.stringify(listino);
 
@@ -144,12 +191,6 @@ $(document).ready(function() {
 
 			var listino = new Object();
             listino.nome = $('#nome').val();
-            var listinoRiferimentoId = $('#listinoRiferimento option:selected').val();
-            if(listinoRiferimentoId != null && listinoRiferimentoId != '' && listinoRiferimentoId != '-1'){
-                var listinoRiferimento = new Object();
-                listinoRiferimento.id = listinoRiferimentoId;
-                listino.listinoRiferimento = listinoRiferimento;
-            }
             listino.tipologia = $('input[name="tipologia"]:checked').val();
 
 			var listinoJson = JSON.stringify(listino);
@@ -191,30 +232,6 @@ $.fn.extractIdListinoFromUrl = function(){
     }
 }
 
-$.fn.getListiniRiferimento = function(idListino){
-	$.ajax({
-		url: baseUrl + "listini",
-		type: 'GET',
-		dataType: 'json',
-		success: function(result) {
-			if(result != null && result != undefined && result != ''){
-				$.each(result, function(i, item){
-					if(idListino != null && idListino != undefined && idListino != ''){
-					    if(idListino != item.id){
-					        $('#listinoRiferimento').append('<option value="'+item.id+'">'+item.nome+'</option>');
-					    }
-					} else {
-					    $('#listinoRiferimento').append('<option value="'+item.id+'">'+item.nome+'</option>');
-					}
-				});
-			}
-		},
-		error: function(jqXHR, textStatus, errorThrown) {
-			console.log('Response text: ' + jqXHR.responseText);
-		}
-	});
-}
-
 $.fn.getListino = function(idListino){
 
 	var alertContent = '<div id="alertListinoContent" class="alert alert-danger alert-dismissible fade show" role="alert">';
@@ -233,9 +250,6 @@ $.fn.getListino = function(idListino){
 
 			$('#hiddenIdListino').attr('value', result.id);
 			$('#nome').attr('value', result.nome);
-            if(result.listinoRiferimento != null && result.listinoRiferimento != undefined){
-                $('#listinoRiferimento option[value=' + result.listinoRiferimento.id +']').attr('selected', true);
-            }
             if(result.tipologia == 'BASE'){
                 $('#tipologiaBase').attr('checked', true);
             } else {
